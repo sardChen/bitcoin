40a9be45acf382916acee89ffbf9a23412baae96
131250106 <chenyuan17@mails.tsinghua.edu.cn>
2018-12-25 17:05:37 +0800

    upload command

--- command.py
+++ command.py
@@ -3,13 +3,13 @@ import os
 from utils import cur_path
 
 
-def xtermCMD(ip1, port1, ip2, port2):
+def xtermCMD(ip1, port1, ip2, port2, mode, nodeType="normal"):
     name="Host_%d: "
 
     file = "setupNode.py"
     file = os.path.join(cur_path, file)
 
-    args = "%s %d %s %d" % (ip1,port1,ip2,port2)
+    args = "%s %d %s %d %s %s" % (ip1,port1,ip2,port2,mode,nodeType)
 
     name = "%s %s %d" % (name, ip1, port1)
 
